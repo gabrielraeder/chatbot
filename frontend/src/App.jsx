54d750e6d58a85e41ef5e674ff267e833712d21b
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-08-30 17:45:56 -0300

    button options

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import {
-  startCheck, usernameCheck, passwordCheck, loanResponse, loanChoices,
+  startCheck, usernameCheck, passwordCheck, loanChoices,
 } from './utils/inputCheck';
 import csvDownload, { addData } from './utils/csvDownload';
 import Header from './components/Header';
@@ -13,6 +13,8 @@ function App() {
   const [password, setPassword] = useState('');
   const [started, setStarted] = useState(false);
   const [loanChat, setLoanChat] = useState(false);
+  const [options, setOptions] = useState('Here are your options:');
+  const [loanOptions, setLoanOptions] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -27,70 +29,69 @@ function App() {
   }, 500);
 
   const handleFormSubmit = (event) => {
-    event.preventDefault();
-    if (inputValue.trim() === '') {
+    setLoanOptions(false);
+    const value = event.target.value || inputValue;
+    const content = event.target.textContent;
+
+    if (value.trim() === '' && !value) {
       setInputValue('');
       return;
     }
 
-    addMessage(inputValue, 'user');
+    if (loanChat && [1, 2, 3].includes(+value)) addMessage(content, 'user');
+    else addMessage(value, 'user');
+
+    event.preventDefault();
     setInputValue('');
 
-    if (inputValue.toLowerCase().includes('goodbye')) {
-      botResponds('Goodbye to you too!');
-      return;
-    }
-
     if (!started) {
-      const check = startCheck(inputValue, setStarted);
+      const check = startCheck(value, setStarted);
       botResponds(check);
       return;
     }
     if (!username) {
-      const userCheck = usernameCheck(inputValue, setUsername);
+      const userCheck = usernameCheck(value, setUsername);
       botResponds(userCheck);
       return;
     }
     if (!password) {
-      const passCheck = passwordCheck(inputValue, setPassword);
+      const passCheck = passwordCheck(value, setPassword);
       botResponds(passCheck);
       return;
     }
 
-    if (inputValue.toLowerCase().includes('loan')) {
-      loanResponse(botResponds);
+    if (value.toLowerCase().includes('loan')) {
+      setLoanOptions(true);
       setLoanChat(true);
       return;
     }
 
-    if (inputValue.toLowerCase().includes('yes') && ['2', '3'].includes(messages[messages.length - 2].content)) {
+    if (value.toLowerCase().includes('yes') && loanChat) {
       botResponds('Great! One of our support assistants will shortly contact you.');
+      setOptions('May I assist you in something else?');
+      setLoanOptions(true);
       return;
     }
 
-    if (loanChat && [1, 2, 3].includes(+inputValue)) {
-      loanChoices(inputValue, botResponds);
+    if (loanChat && [1, 2, 3].includes(+value)) {
+      loanChoices(value, botResponds);
       return;
     }
 
     const regex = /[a-z0-9]+@[a-z]+\.[a-z]/;
-    if (regex.test(inputValue)) {
+    if (regex.test(value)) {
       botResponds('Great! We will send you an email with more informations shortly.');
-      botResponds('May I assist you in something else?');
+      setOptions('May I assist you in something else?');
+      setLoanChat(true);
       return;
     }
 
-    if (!regex.test(inputValue)) {
+    if (!regex.test(value)) {
       botResponds('Invalid email, please try again...');
       return;
     }
 
     setLoanChat(false);
-
-    // if (inputValue.toLowerCase().includes('goodbye')) {
-    //   botResponds('Goodbye to you too!');
-    //   return;
-    // }
 
     botResponds('ChatGPT integration here');
   };
@@ -101,8 +102,10 @@ function App() {
       chatContainer.scrollTop = chatContainer.scrollHeight;
     }
 
-    if (messages[messages.length - 1]?.content.toLowerCase().includes('goodbye to you too!')) {
+    if (messages[messages.length - 1]?.content.toLowerCase().includes('goodbye')) {
+      setLoanOptions(false);
       setTimeout(() => {
+        addMessage('Ok! Goodbye to you too!', 'bot');
         addData(messages);
         setMessages([]);
         setStarted(false);
@@ -125,6 +128,21 @@ function App() {
               </div>
             </div>
           ))}
+          {
+            loanOptions && (
+              <div
+                className="message-container bot"
+              >
+                <div className="message bot-message loanBtns">
+                  <h5>{options}</h5>
+                  <button type="button" value="1" onClick={handleFormSubmit}>Do you want to apply for a loan?</button>
+                  <button type="button" value="2" onClick={handleFormSubmit}>Loan conditions</button>
+                  <button type="button" value="3" onClick={handleFormSubmit}>Help</button>
+                  <button type="button" onClick={() => addMessage('goodbye', 'user')}>End conversation</button>
+                </div>
+              </div>
+            )
+          }
         </div>
         <form className="chat-input" onSubmit={handleFormSubmit}>
           <input
